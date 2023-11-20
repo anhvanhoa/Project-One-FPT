1774f86ed03b73bff8533fb8f665d98d138c6fec
hoann412 <hoanthph40189.edu.vn>
2023-11-20 14:10:15 +0700

    change con troller

--- controllers/admin/CategoriesController.php
+++ controllers/admin/CategoriesController.php
@@ -1,13 +1,20 @@
 <?php
 function controller_categories($req)
 {
-    return viewAdmin("categories", []);
+    $categories = $req->categoriesService->findAll();
+    return viewAdmin("categories", ['categories'=>$categories]);
 }
+
+
 function controller_add_categories($req)
 {
+
     return viewAdmin("addCategory", []);
 }
+
+
 function controller_edit_categories($req)
 {
+    
     return viewAdmin("editCategory", []);
 }
--- views/admin/categories.php
+++ views/admin/categories.php
@@ -47,7 +47,10 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr class="bg-white hover:bg-gray-50 ">
+                            <?php
+                                foreach($categories as $category) {
+                            ?>
+                             <tr class="bg-white hover:bg-gray-50 ">
                                 <td class="w-4 p-4">
                                     <div class="flex items-center">
                                         <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 rounded focus:ring-blue-500  ring-offset-gray-800 focus:ring-offset-gray-800 focus:ring-2  border-gray-600">
@@ -55,19 +58,22 @@
                                     </div>
                                 </td>
                                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                                    1
+                                    <?= $category['id'] ?>
                                 </th>
                                 <td class="px-6 py-4">
-                                    Silver
+                                    <?= $category['name_category'] ?>
                                 </td>
                                 <td class="px-6 py-4">
-                                    sofa-1.jpg
+                                    <?= $category['image'] ?>
                                 </td>
                                 <td class="flex items-center px-6 py-4">
                                     <a href="/admin?act=edit-category" class="font-medium text-blue-600  hover:underline">Edit</a>
                                     <a href="#" class="font-medium text-red-600 hover:underline ms-3">Remove</a>
                                 </td>
                             </tr>
+                            <?php
+                                }
+                            ?>
                         </tbody>
                     </table>
                 </div>
